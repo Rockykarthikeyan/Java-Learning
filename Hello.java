7372fa5558c4b6eb42b4bc0a5a67fc25bcb44c8a
rockykarthikeyan <kxrthikeyan06@gmail.com>
2025-08-09 12:33:00 +0530

    First commit

---
+++ Hello.java
@@ -0,0 +1,5 @@
+public Class Hello{
+ public static void main(string args[]){
+   system.out.println("Hello world");
+}
+}
---
+++ Karthi.java
@@ -0,0 +1,8 @@
+public class Karthi{
+ public static void main (String[] args){
+   System.out.println("Hello world");
+   System.out.print("Prathipa");
+   System.out.print(1907);
+}
+}
+  
