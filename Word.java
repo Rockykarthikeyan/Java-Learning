da28f73a61b0a5e0ba697dc6dfe638a063bed7df
rockykarthikeyan <kxrthikeyan06@gmail.com>
2025-08-09 12:37:47 +0530

    second commit

---
+++ Word.java
@@ -0,0 +1,9 @@
+public class Word{
+ public static void main(String args[]){
+   System.out.println("Family Members Name:");
+   System.out.println("Father:Dasaradan");
+   System.out.println("Mother-Sumithra");
+   System.out.println("Brother-Karthikeyan");
+   System.out.println("Sister-Dhanushkaa");
+}
+}
